c4f14f2553ecaf244e563ba5b4578c56e5890743
Hiep Nguyen <thienhiep1711@users.noreply.github.com>
2018-11-15 09:33:50 +0700

    commit contact page

--- src/components/Contact/ContactContent.jsx
+++ src/components/Contact/ContactContent.jsx
@@ -3,8 +3,16 @@ import React, { Component } from 'react';
 class ContactContent extends Component {
   render() {
     return (
-      <div>
-        content
+      <div className="contact__info">
+        <h2 className="contact__info__title">Nguyen Thien Hiep</h2>
+        <div className="contact__info__detail">
+          <ul>
+            <li className="position">Front End Devevloper</li>
+            <li><strong>Email:</strong><span>thienhiep171193@gmail.com</span></li>
+            <li><strong>Phone:</strong><span>0915280986</span></li>
+            <li><strong>Địa chỉ:</strong><span>Bắc Từ Liêm - Hà Nội</span></li>
+          </ul>
+        </div>
       </div>
     );
   }
--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -3,8 +3,25 @@ import React, { Component } from 'react';
 class ContactForm extends Component {
   render() {
     return (
-      <div>
-        form
+      <div className="contact__form">
+        <h2 className="contact__info__title">Yêu cầu liên hệ</h2>
+        <form action="#">
+          <div class="form-group">
+              <input type="text" class="form-control" id="iptName" placeholder="Họ và tên" />
+          </div>
+          <div class="form-group">
+              <input type="text" class="form-control" id="iptEmail" placeholder="Email" />
+          </div>
+          <div class="form-group">
+              <input type="text" class="form-control" id="iptPhone" placeholder="Số điện thoại" />
+          </div>
+          <div class="form-group">
+              <input type="text" class="form-control" id="iptCompany" placeholder="Công ty" />
+          </div>
+          <div class="form-group">
+              <button type="submit" class="btn btn-primary">Gửi yêu cầu</button>
+          </div>
+        </form>
       </div>
     );
   }
